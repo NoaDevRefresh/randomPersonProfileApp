37c77e20afc18e49344cbfcb227d1f26626381ce
Noa Dev <noadev.code@gmail.com>
2021-07-19 13:13:58 +0200

    Add config file and set up project

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const { mainModule } = require('process');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    resolve: {
+        extensions: ['.js']
+    }
+}
